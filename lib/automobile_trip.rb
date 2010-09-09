9789c60beb1fae4d463d340b481af8122ba67d65
Derek Kastner <dkastner@gmail.com>
2010-09-09 16:26:43 -0400

    AutomobileTrip.automobile_trip_model is not needed anymore

--- lib/automobile_trip.rb
+++ lib/automobile_trip.rb
@@ -3,15 +3,5 @@ require 'emitter'
 module BrighterPlanet
   module AutomobileTrip
     extend BrighterPlanet::Emitter
-
-    def self.automobile_trip_model
-      if Object.const_defined? 'AutomobileTrip'
-        ::AutomobileTrip
-      elsif Object.const_defined? 'AutomobileTripRecord'
-        AutomobileTripRecord
-      else
-        raise 'There is no automobile trip model'
-      end
-    end
   end
 end
