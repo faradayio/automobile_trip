fa487154356e3664c9886b984eb553c46b09e073
Ian Hough <ian@brighterplanet.com>
2010-09-10 11:10:16 -0700

    Basic emission calculations

--- lib/automobile_trip/carbon_model.rb
+++ lib/automobile_trip/carbon_model.rb
@@ -7,9 +7,29 @@ module BrighterPlanet
       def self.included(base)
         base.extend ::Leap::Subject
         base.decide :emission, :with => :characteristics do
-          committee :emission do
-            quorum 'from fuel consumed and emission factor' do
-              16.0
+          committee :emission do # returns kg CO2e
+            quorum 'from fuel use and emission factor', :needs => [:fuel_use, :emission_factor] do |characteristics|
+              characteristics[:fuel_use] * characteristics[:emission_factor]
+            end
+            
+            quorum 'default' do
+              raise "The emission committee's default quorum should never be called"
+            end
+          end
+          
+          committee 'emission_factor' do # returns lbs CO2e / l
+            # quorum 'from fuel type', :needs => :fuel_type do |characteristics|
+            #   characteristics[:fuel_type].emission_factor
+            # end
+            
+            quorum 'default' do
+              base.fallback.emission_factor
+            end
+          end
+          
+          committee :fuel_use do # returns l
+            quorum 'default' do
+              base.fallback.fuel_use
             end
           end
         end
