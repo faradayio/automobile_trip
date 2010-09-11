e940cbe134439cb0eb04dbd8bdd667527c5ca367
Ian Hough <ian@brighterplanet.com>
2010-09-10 17:12:03 -0700

    Fix fallback declaration

--- lib/test_support/automobile_trip_record.rb
+++ lib/test_support/automobile_trip_record.rb
@@ -7,6 +7,6 @@ class AutomobileTripRecord < ActiveRecord::Base
   include Sniff::Emitter
   include BrighterPlanet::AutomobileTrip
 
-  falls_back_on :emission_factor => 20.556.pounds_per_gallon.to(:kilograms_per_litre) # from footprint model gasoline car CO2e / gallon
-  falls_back_on :fuel_use => 57.0 # based on wikianswers average tank size of 15 gallons
+  falls_back_on :emission_factor => 20.556.pounds_per_gallon.to(:kilograms_per_litre), # from footprint model gasoline car CO2e / gallon
+                :fuel_use        => 57.0                                               # based on wikianswers average tank size of 15 gallons
 end
