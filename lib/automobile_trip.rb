d00ba379a022dbb4960644491e14963252de0f50
Ian Hough <ian@brighterplanet.com>
2011-02-14 09:47:32 -0800

    Document the boundary of Automobile Trip emissions

--- lib/automobile_trip.rb
+++ lib/automobile_trip.rb
@@ -3,5 +3,6 @@ require 'emitter'
 module BrighterPlanet
   module AutomobileTrip
     extend BrighterPlanet::Emitter
+    scope 'The automobile trip emission estimate includes CO2 emissions from non-biogenic fuel use, CH4 and N2O emissions from all fuel use, and fugitive HFC emissions from air conditioning.'
   end
 end
