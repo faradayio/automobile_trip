6c442ece1ea1f1c035f4f90e151dc767d97dd86b
Seamus Abshere <seamus@abshere.net>
2011-09-27 10:14:00 -0500

    fix

--- lib/automobile_trip/data.rb
+++ lib/automobile_trip/data.rb
@@ -2,27 +2,27 @@ module BrighterPlanet
   module AutomobileTrip
     module Data
       def self.included(base)
-        col :date, :type => :date   
-        col :country_iso_3166_code 
-        col :make_name 
-        col :make_year_name 
-        col :make_model_name 
-        col :make_model_year_name 
-        col :make_model_year_variant_row_hash 
-        col :size_class_name 
-        col :hybridity, :type => :boolean
-        col :urbanity, :type => :float  
-        col :hybridity_multiplier, :type => :float  
-        col :fuel_efficiency, :type => :float  
-        col :speed, :type => :float  
-        col :city_speed, :type => :float  
-        col :highway_speed, :type => :float  
-        col :duration, :type => :float  
-        col :origin 
-        col :destination 
-        col :distance, :type => :float  
-        col :fuel_use, :type => :float  
-        col :automobile_fuel_name 
+        base.col :date, :type => :date   
+        base.col :country_iso_3166_code 
+        base.col :make_name 
+        base.col :make_year_name 
+        base.col :make_model_name 
+        base.col :make_model_year_name 
+        base.col :make_model_year_variant_row_hash 
+        base.col :size_class_name 
+        base.col :hybridity, :type => :boolean
+        base.col :urbanity, :type => :float  
+        base.col :hybridity_multiplier, :type => :float  
+        base.col :fuel_efficiency, :type => :float  
+        base.col :speed, :type => :float  
+        base.col :city_speed, :type => :float  
+        base.col :highway_speed, :type => :float  
+        base.col :duration, :type => :float  
+        base.col :origin 
+        base.col :destination 
+        base.col :distance, :type => :float  
+        base.col :fuel_use, :type => :float  
+        base.col :automobile_fuel_name 
       end
     end
   end
