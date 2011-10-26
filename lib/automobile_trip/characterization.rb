cd9913e17876adad5420c1de7d3e7e438547e41e
Ian Hough <ijhough@gmail.com>
2011-10-26 22:07:38 +0200

    Tweaks

--- lib/automobile_trip/characterization.rb
+++ lib/automobile_trip/characterization.rb
@@ -3,7 +3,6 @@ module BrighterPlanet
     module Characterization
       def self.included(base)
         base.characterize do
-          has :date
           has :country
           has :make
           has :make_year
@@ -11,18 +10,19 @@ module BrighterPlanet
           has :make_model_year
           has :make_model_year_variant
           has :size_class
+          has :automobile_fuel  # can't call this 'fuel' or else sniff thinks it refers to Fuel not AutomobileFuel
+          has :date
           has :hybridity
           has :urbanity
-          has :fuel_efficiency, :measures => Measurement::BigLengthPerVolume
-          has :speed, :measures => Measurement::BigSpeed
-          has :city_speed, :measures => Measurement::BigSpeed
-          has :highway_speed, :measures => Measurement::BigSpeed
-          has :duration, :measures => :time
+          has :city_speed,      :measures => Measurement::BigSpeed
+          has :highway_speed,   :measures => Measurement::BigSpeed
+          has :speed,           :measures => Measurement::BigSpeed
+          has :duration,        :measures => :time
           has :origin
           has :destination
-          has :distance, :measures => Measurement::BigLength
-          has :fuel_use, :measures => Measurement::Volume
-          has :automobile_fuel # don't call this fuel b/c then if you specify fuel.name in tests sniff will try to look it up in the fuels fixture, not automobile_fuels
+          has :distance,        :measures => Measurement::BigLength
+          has :fuel_efficiency, :measures => Measurement::BigLengthPerVolume
+          has :fuel_use,        :measures => Measurement::Volume
         end
       end
     end
--- lib/automobile_trip/data.rb
+++ lib/automobile_trip/data.rb
@@ -2,26 +2,26 @@ module BrighterPlanet
   module AutomobileTrip
     module Data
       def self.included(base)
-        base.col :date, :type => :date   
-        base.col :country_iso_3166_code 
-        base.col :make_name 
-        base.col :make_year_name 
-        base.col :make_model_name 
-        base.col :make_model_year_name 
-        base.col :make_model_year_variant_row_hash 
-        base.col :size_class_name 
-        base.col :hybridity, :type => :boolean
-        base.col :urbanity, :type => :float  
-        base.col :fuel_efficiency, :type => :float  
-        base.col :speed, :type => :float  
-        base.col :city_speed, :type => :float  
-        base.col :highway_speed, :type => :float  
-        base.col :duration, :type => :float  
-        base.col :origin 
-        base.col :destination 
-        base.col :distance, :type => :float  
-        base.col :fuel_use, :type => :float  
-        base.col :automobile_fuel_name 
+        base.col :country_iso_3166_code
+        base.col :make_name
+        base.col :make_year_name
+        base.col :make_model_name
+        base.col :make_model_year_name
+        base.col :make_model_year_variant_row_hash
+        base.col :size_class_name
+        base.col :automobile_fuel_name
+        base.col :date,            :type => :date
+        base.col :hybridity,       :type => :boolean
+        base.col :urbanity,        :type => :float
+        base.col :city_speed,      :type => :float
+        base.col :highway_speed,   :type => :float
+        base.col :speed,           :type => :float
+        base.col :duration,        :type => :float
+        base.col :origin
+        base.col :destination
+        base.col :distance,        :type => :float
+        base.col :fuel_efficiency, :type => :float
+        base.col :fuel_use,        :type => :float
       end
     end
   end
