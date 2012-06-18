0448bcfd5a11f7d1c29d802e1695e15f7bad18cc
Ian Hough <ijhough@gmail.com>
2012-06-18 16:17:12 +0200

    Update description of emission calculation scope

--- lib/automobile_trip.rb
+++ lib/automobile_trip.rb
@@ -5,6 +5,6 @@ require 'geokit'
 module BrighterPlanet
   module AutomobileTrip
     extend BrighterPlanet::Emitter
-    scope 'The automobile trip emission estimate is the total anthropogenic emissions from fuel and air conditioning used by the automobile during the trip. It includes CO2 emissions from combustion of non-biogenic fuel, CH4 and N2O emissions from combustion of all fuel, and fugitive HFC emissions from air conditioning.'
+    scope 'The automobile trip emission estimate is the total anthropogenic emissions from fuel and air conditioning used by the automobile during the trip. It includes CO2 emissions from combustion of non-biogenic fuel, CH4 and N2O emissions from combustion of all fuel, and fugitive HFC emissions from air conditioning. For vehicles powered by grid electricity it includes the emissions from generating that electricity.'
   end
 end
