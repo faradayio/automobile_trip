d09d85601b834f74624a919f3f8b4e9b05748782
Derek Kastner <dkastner@gmail.com>
2012-08-03 17:34:26 -0500

    Fix RSpec mocks

--- features/step_definitions/automobile_trip_steps.rb
+++ features/step_definitions/automobile_trip_steps.rb
@@ -1,8 +1,12 @@
+require 'rspec/mocks'
+RSpec::Mocks.setup self
+include RSpec::Mocks::ExampleMethods
+
 Given /^mapquest determines the distance in miles to be "([^\"]*)"$/ do |distance|
   if distance.present?
-    mockquest = mock MapQuestDirections, :status => 0, :xml => "<distance>" + distance.to_s + "</distance>"
+    mockquest = double MapQuestDirections, :status => 0, :xml => "<distance>" + distance.to_s + "</distance>"
   else
-    mockquest = mock MapQuestDirections, :status => 601, :xml => "<distance>0</distance>"
+    mockquest = double MapQuestDirections, :status => 601, :xml => "<distance>0</distance>"
   end
   MapQuestDirections.stub!(:new).and_return mockquest
 end
@@ -11,7 +15,7 @@ Given /^the geocoder will encode the (.*) as "(.*)"$/ do |component, location|
   @expectations << lambda do
     components = @characteristics ? @characteristics : @activity_hash
     component_value = components[component.to_sym].to_s
-    code = mock Object, :success => true, :ll => location
+    code = double Object, :success => true, :ll => location
     Geokit::Geocoders::MultiGeocoder.stub!(:geocode).with(component_value).and_return code
   end
 end
@@ -20,7 +24,7 @@ Given /^the geocoder will fail to encode the (.*)$/ do |component|
   @expectations << lambda do
     components = @characteristics ? @characteristics : @activity_hash
     component_value = components[component.to_sym].to_s
-    code = mock Object, :success => false
+    code = double Object, :success => false
     Geokit::Geocoders::MultiGeocoder.should_receive(:geocode).with(component_value).and_return code
   end
 end
