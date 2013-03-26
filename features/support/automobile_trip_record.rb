32977c7ce099debeae62d8b5d27d3424221ecff0
Seamus Abshere <seamus@abshere.net>
2013-03-25 19:53:29 -0500

    maybe a better place to set this up

--- features/support/automobile_trip_record.rb
+++ features/support/automobile_trip_record.rb
@@ -4,17 +4,3 @@ class AutomobileTripRecord < ActiveRecord::Base
   include BrighterPlanet::Emitter
   include BrighterPlanet::AutomobileTrip
 end
-
-require 'geocoder'
-class GeocoderWrapper
-  def geocode(input)
-    if res = ::Geocoder.search(input).first
-      {
-        latitude:  res.coordinates[0],
-        longitude: res.coordinates[1],
-      }
-    end
-  end
-end
-
-BrighterPlanet::AutomobileTrip.geocoder = GeocoderWrapper.new
--- features/support/env.rb
+++ features/support/env.rb
@@ -4,3 +4,16 @@ require 'sniff'
 Sniff.init File.expand_path('../../..', __FILE__),
   :cucumber => true,
   :logger => false # change this to $stderr to see database activity
+
+require 'geocoder'
+class GeocoderWrapper
+  def geocode(input)
+    if res = ::Geocoder.search(input).first
+      {
+        latitude:  res.coordinates[0],
+        longitude: res.coordinates[1],
+      }
+    end
+  end
+end
+BrighterPlanet::AutomobileTrip.geocoder = GeocoderWrapper.new
